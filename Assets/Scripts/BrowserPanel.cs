9f3a9fc9bc5b31dac5943d623d35d8c76c68fc5a
Hernan <zweihander11@gmail.com>
2017-07-04 17:25:58 -0400

    IMplementacion panel obras
    Obras se muestran y se cargan pero no de acuerdo a las categorías

--- Assets/Scripts/BrowserPanel.cs
+++ Assets/Scripts/BrowserPanel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BrowserPanel : MonoBehaviour 
 {
@@ -32,20 +33,48 @@ public class BrowserPanel : MonoBehaviour
 			foreach (Tooltip tt in genericMenu.intersectTooltip) 
 			{
 
+				for (int i = genericMenu.intersectTooltip.Count; i <= displayPieceCount; i++) // Hide the remaining pieces
+				{
+					if (i != 0 && i < displayPieceCount) 
+					{
+						print (genericMenu.intersectTooltip.Count + " I:" + i);
+						genericMenu.rightPanel.transform.GetChild (i+1).gameObject.SetActive (false);
+					}
+
+
+				}
+
+
+				foreach (Transform obj in genericMenu.rightPanel.transform)
+				{
+
+					if (obj.gameObject.tag == "piece" && obj.gameObject.activeSelf) 
+					{
+
+						print ("AddedPiece Listener");
+						obj.gameObject.AddComponent<EventTrigger>();
+						EventTrigger.Entry entrypiece = new EventTrigger.Entry ();
+						entrypiece.eventID = EventTriggerType.PointerClick;
+						entrypiece.callback.AddListener ((data) =>  genericMenu.appController.InitScene(tt.poiID, tt.floorID) );
+						obj.gameObject.GetComponent<EventTrigger> ().triggers.Add (entrypiece);
 
 
 
+					}
 
+
+				}
+
+				//TODO add support for extra pages
+				//TODO add support for thumbnail show
 			}
 
-			for (int i = genericMenu.intersectTooltip.Count; i <= displayPieceCount; i++) // Hide the remaining pieces
-			{
 
 
-				genericMenu.rightPanel.transform.GetChild (i).gameObject.SetActive(false);
-			
 
-			}
+
+
+
 
 
 
